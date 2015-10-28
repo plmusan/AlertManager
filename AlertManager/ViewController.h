5a885ba7f08b3f39004e8d17e8d7b21c1e283326
plmusan <xWang@MAC041.local>
2015-10-28 16:47:34 +0800

    Initial Commit

---
+++ AlertManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlertManager
+//
+//  Created by x.wang on 15/10/28.
+//  Copyright (c) 2015年 x.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
